66507d912f0080a5f572d4bc2582012ee3d25f78
Niccolo Alfredo <niccolo@MacBook-Pro-di-Niccolo.local>
2025-07-16 12:38:02 +0200

    Creazione ping-pong + aggiunta nodi

---
+++ ping-pong/provision/node_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "--- Provisioning Node1: Docker Installation ---"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker vagrant # Aggiungi l'utente 'vagrant' al gruppo docker per evitare 'sudo'
+sudo systemctl enable docker
+sudo systemctl start docker
+echo "--- Node1 Provisioning Complete ---"
